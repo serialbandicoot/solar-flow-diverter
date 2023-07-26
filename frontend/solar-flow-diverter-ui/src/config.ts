3a54a97edca4c1bc83acb0aa21b184457a83c707
serialbandicoot <samtreweek@gmail.com>
2023-07-26 21:43:23 +0100

    wrong url for api

--- frontend/solar-flow-diverter-ui/src/config.ts
+++ frontend/solar-flow-diverter-ui/src/config.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = 'http://api.solar-flow-diverter.uk/api';
-export const apiUrl = 'http://localhost:5000/api';
+export const apiUrl = 'http://api.solar-flow-diverter.uk/api';
+// export const apiUrl = 'http://localhost:5000/api';
