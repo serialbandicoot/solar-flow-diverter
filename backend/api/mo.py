2732c7ff6244c97d41398067edf037596bd06edb
serialbandicoot <samtreweek@gmail.com>
2023-07-29 13:19:54 +0100

    add logger to mo

--- backend/api/mo.py
+++ backend/api/mo.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import datetime as dt
 import time
+import logging
 
 from scheduler import Scheduler
 from config.config import load_config
@@ -8,8 +10,17 @@ from src import metoffer
 
 EPOC = 5
 
+# Logger Stuff!
+log_file = '/var/log/mo.log'
+logger = logging.getLogger('mo_logger')
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_file)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 def mo():
-    print(f"*** Get 5 Day Forecast - {dt.datetime.now()} ***")
+    logger.info(f"*** Get 5 Day Forecast - {dt.datetime.now()} ***")
     config_values = load_config()
 
     M = metoffer.MetOffer(config_values["met_office_api_key"])
