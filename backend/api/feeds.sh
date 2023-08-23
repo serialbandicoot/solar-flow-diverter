0a369bc3a475981733818b13bb334c6ce8622829
serialbandicoot <samtreweek@gmail.com>
2023-08-23 19:23:11 +0100

    Update feeds.sh

--- backend/api/feeds.sh
+++ backend/api/feeds.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X GET c/v1/create_pv
-curl -X POST -H "Content-Type: application/json" -d '{"step": "5d"}' http://localhost:5000/v1/weather
+curl -X GET https://api.solar-flow-diverter.uk/v1/create_pv
+curl -X POST -H "Content-Type: application/json" -d '{"step": "5d"}' https://api.solar-flow-diverter.uk/v1/weather
